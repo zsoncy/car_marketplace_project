1be0fe4f4b0daa4bfc04893d3b82899f1114306d
Kulcsár Gerzson <zsoncy@mailbox.unideb.hu>
2025-11-16 00:22:12 +0100

    setting up the PostgreSQL database using docker

---
+++ src/main/java/hu/inf/unideb/CarMarketplaceApp.java
@@ -0,0 +1,13 @@
+package hu.inf.unideb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarMarketplaceApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarMarketplaceApp.class, args);
+    }
+
+}
