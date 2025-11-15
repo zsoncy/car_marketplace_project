8271d6c59412af20a96761a27779908c00718d86
Kulcsár Gerzson <zsoncy@mailbox.unideb.hu>
2025-11-15 23:21:04 +0100

    implementing the repository interfaces

---
+++ src/main/java/hu/inf/unideb/repository/CarRepo.java
@@ -0,0 +1,9 @@
+package hu.inf.unideb.repository;
+
+import hu.inf.unideb.model.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CarRepo extends JpaRepository<Car, String> {
+}
---
+++ src/main/java/hu/inf/unideb/repository/UserRepo.java
@@ -0,0 +1,9 @@
+package hu.inf.unideb.repository;
+
+import hu.inf.unideb.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Long> {
+}
