0f1f22fd0d3d51013215080d94fd8e384f5ec802
Kulcsár Gerzson <zsoncy@mailbox.unideb.hu>
2025-11-30 21:07:58 +0100

    implementing the required login Dto models

---
+++ src/main/java/hu/inf/unideb/DTOs/LoginRequestDto.java
@@ -0,0 +1,15 @@
+package hu.inf.unideb.DTOs;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LoginRequestDto {
+    private String username;
+    private String password;
+}
---
+++ src/main/java/hu/inf/unideb/DTOs/LoginResponseDto.java
@@ -0,0 +1,16 @@
+package hu.inf.unideb.DTOs;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LoginResponseDto {
+    private String username;
+    private String role;
+    private String accessToken;
+}
