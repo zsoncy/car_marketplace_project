21260b86a5ba5f3fdf5b1225cfc1442981d8ee07
Kulcsár Gerzson <zsoncy@mailbox.unideb.hu>
2025-11-30 21:07:51 +0100

    implementing the required register Dto models

---
+++ src/main/java/hu/inf/unideb/DTOs/RegisterRequestDto.java
@@ -0,0 +1,16 @@
+package hu.inf.unideb.DTOs;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RegisterRequestDto {
+    private String username;
+    private String password;
+}
+
